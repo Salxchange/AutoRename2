ed9429f185bfa893cf58043de9a3f6b9cfaf48d2
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-02 03:07:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "22418774")
     API_HASH  = os.environ.get("API_HASH", "d8c8dab274f9a811814a6a96d044028e")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6853799265:AAHAia3VERsC01OlMEcgpNrIgWaC_8mnw1o") 
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","Roku")     
@@ -17,7 +17,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/3408344d69952d7a739e9.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1966867320').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "Rokubotz") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002020039437"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870788712"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
@@ -33,7 +33,7 @@ class Txt(object):
     """
         
     START_TXT = """𝖧𝖾𝗅𝗅𝗈 {}
-𝖧𝗂 {username} 𝗂 𝖺𝗆 <a href=http://t.me/FuZionX_SD>𝖥𝗎𝖹𝗂𝗈𝗇𝖷 𝖲𝖣</a>, 𝖠𝗇 𝖠𝗎𝗍𝗈 𝖱𝖾𝗇𝖺𝗆𝖾 𝖡𝗈𝗍,
+𝖧𝗂 {username} 𝗂 𝖺𝗆 <a href=http://t.me/FuZionX_SD_Bot>𝖥𝗎𝖹𝗂𝗈𝗇𝖷 𝖲𝖣</a>, 𝖠𝗇 𝖠𝗎𝗍𝗈 𝖱𝖾𝗇𝖺𝗆𝖾 𝖡𝗈𝗍,
 𝖳𝗁𝖺𝗍 𝖱𝖾𝗇𝖺𝗆𝖾𝗌 𝖠𝗇𝗂𝗆𝖾 𝖥𝗂𝗅𝖾𝗌 𝖳𝗈 𝖳𝗁𝖾 𝖥𝗈𝗋𝗆𝖺𝗍 𝖳𝗈 𝖲𝖾𝗍 . "
 
 𝖳𝗈 𝖲𝖾𝖾 𝖬𝗒 𝖥𝗎𝗇𝖼𝗍𝗂𝗈𝗇𝗌, 𝖴𝗌𝖾 𝖳𝗁𝖾 𝖡𝗎𝗍𝗍𝗈𝗇𝗌 𝖡𝖾𝗅𝗈𝗐"
@@ -44,13 +44,13 @@ class Txt(object):
     """
     
     ABOUT_TXT = f"""<b>╭───────────⍟
-├🤖 𝖬𝗒 𝗇𝖺𝗆𝖾: <a href=http://t.me/FuZionX_SD>𝖥𝗎𝖹𝗂𝗈𝗇𝖷 𝖲𝖣</a>
+├🤖 𝖬𝗒 𝗇𝖺𝗆𝖾: <a href=http://t.me/FuZionX_SD_Bot>𝖥𝗎𝖹𝗂𝗈𝗇𝖷 𝖲𝖣</a>
 ├📕 𝖫𝗂𝖻𝗋𝖺𝗋𝗒: <a href=https://github.com/pyrogram>𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆</a>
 ├✏️ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾: <a href=https://www.python.org>𝖯𝗒𝗍𝗁𝗈𝗇3</a>
 ├💾 𝖣𝖺𝗍𝖺𝖡𝖺𝗌𝖾: <a href=https://cloud.mongodb.com>𝖬𝗈𝗇𝗀𝗈𝖣𝖡</a>
 ├📊 𝖡𝗎𝗂𝗅𝖽 𝖵𝖾𝗋𝗌𝗂𝗈𝗇: `OBI V1.7.0`
 ├🔗 𝖦𝗂𝗍𝖧𝗎𝖻: <a href=https://github.com/SilentDemonSD>𝖲𝗂𝗅𝖾𝗇𝗍𝖣𝖾𝗆𝗈𝗇𝖲𝖣</a>
-├📧 𝖢𝗈𝗇𝗍𝖺𝖼𝗍: <a href=https://telegram.me/Mystery SD>𝖲𝗂𝗅𝖾𝗇𝗍𝖣𝖾𝗆𝗈𝗇</a>
+├📧 𝖢𝗈𝗇𝗍𝖺𝖼𝗍: <a href=https://telegram.me/MysterySD>𝖲𝗂𝗅𝖾𝗇𝗍𝖣𝖾𝗆𝗈𝗇</a>
 ╰───────────────⍟ 
 """
 
